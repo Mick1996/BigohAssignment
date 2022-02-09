b4c34eb3a42b9560c952f63b1b4d689b79410e02
Mick1996# <mickykumar51@gmail.com>
2022-02-09 11:35:40 +0530

    Q1 of Assingnment 2

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,42 @@
+﻿class uniqueArray //question 1 of assignment 2
+{
+    public int[] removeDuplicates(int [] values)
+    {
+        int[] arr = new int[values.Length];
+     for(int i=0;i<values.Length;i++)
+        {
+            if (values[i] != -1)
+            {
+                int n = values[i];
+                for (int j = i + 1; j < values.Length; j++)
+                {
+
+                    if (n == values[j])
+                        values[j] = -1;
+                }
+            }
+        }
+         for(int i=0;i<values.Length;i++)
+        {
+            if(values[i]!=-1)
+            {
+              arr[i]=values[i];
+            }
+        }
+          return arr;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        int[] arr = {1,2,5,4,8,9,5,4,2,7};
+        uniqueArray u = new uniqueArray();
+        int[] resultArray=u.removeDuplicates(arr);
+        for(int i=0;i<resultArray.Length;i++)
+        {
+            if(resultArray[i]!=0)
+            Console.WriteLine(resultArray[i]);
+        }
+    }
+}
