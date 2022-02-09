1f9cef3f88506e625b32d31af836f296d1844e87
Mick1996# <mickykumar51@gmail.com>
2022-02-09 11:30:45 +0530

    Q3 of Assignment

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System; //question 3
+class LargestPrime
+{
+    public void getLargestPrime(int num)
+    {
+     for(int i=num-1;i>0;i--)
+        {
+            int k=checkEveryDigitPrime(i);
+            if(k!=0)
+            {
+                Console.WriteLine(i);
+                break;
+            }
+        }
+    }
+    public int checkEveryDigitPrime(int n)
+    {
+       int rem;
+      while(n>0)
+        {
+            rem = n % 10;
+            for(int i=2;i<rem/2;i++)
+            {
+                if (rem % i == 0)
+                {
+                     return 0;
+                }    
+            }
+            n = n / 10;
+        }
+            return 1;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        LargestPrime l = new LargestPrime();
+        l.getLargestPrime(5689);
+    }
+}
+
+
+
+
+
+
+
+
+
+
