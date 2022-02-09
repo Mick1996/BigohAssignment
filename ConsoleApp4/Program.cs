f733c31a49fedb6b4f8abfbf5677dc0ffdc82c70
Mick1996# <mickykumar51@gmail.com>
2022-02-09 11:32:40 +0530

    Q4 of Assignment 1

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions; //question 4
+class validUrl
+{
+    public bool isValid(string url)
+    {
+        //string Pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+        string Pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(.com)";
+        Regex Rgx = new Regex(Pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        return Rgx.IsMatch(url);
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        string str = "http://www.google.com";
+        validUrl v = new validUrl();
+        bool result=v.isValid(str);
+        Console.WriteLine(result);
+    }
+}
