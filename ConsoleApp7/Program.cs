6381cae35af2ae871c736c540ec9fc3adb05e124
Mick1996# <mickykumar51@gmail.com>
2022-02-09 11:40:09 +0530

    Q3 of Assignment 2

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program //question 3 of assignment 2
+{
+    public static void Main()
+    {
+       
+        for(int i=0;i<7;i++)
+        {
+           for(int j=0;j<7;j++)
+            {
+                if (i == j)
+                    Console.Write("\\");
+                else if(i+j==7-1)
+                    Console.Write("\\");
+                else
+                    Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
