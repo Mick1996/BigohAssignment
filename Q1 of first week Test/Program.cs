a3a4c527fed6a64826b1c45d41e235fb9fef161c
Mick1996# <mickykumar51@gmail.com>
2022-02-12 16:04:03 +0530

    q1 of First week test

---
+++ Q1 of first week Test/Program.cs
@@ -0,0 +1,25 @@
+﻿class InterLeavingStrings
+{
+	public void InterLeaving(string s1, int i, string s2, int j, string Value)
+	{
+		if (i == s1.Length && j == s2.Length)
+		{
+			Console.WriteLine(Value);
+		}
+
+		if (i < s1.Length)
+			InterLeaving(s1, i + 1, s2, j, Value + s1[i]);
+
+		if (j < s2.Length)
+			InterLeaving(s1, i, s2, j + 1, Value + s2[j]);
+
+	}
+	public static void Main()
+	{
+
+		string s1 = "AB"; 
+		string s2 = "CD";
+		InterLeavingStrings obj=new InterLeavingStrings();
+		obj.InterLeaving(s1, 0, s2, 0, "");
+	}
+}
