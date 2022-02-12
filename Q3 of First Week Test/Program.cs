64da8525a98da360f50f0e4aa56b89b7b9a7fb7e
Mick1996# <mickykumar51@gmail.com>
2022-02-12 16:32:43 +0530

    q3 of First week Test

---
+++ Q3 of First Week Test/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int[] arr = {8,5,10,7,9,4,15,12,90,13};
+        int []MaxArray=new int[100];
+        int k = 4,p=0,max=0;
+        int c = k - 1;
+        for(int i=0;i<arr.Length-c;i++)
+        {
+           for(int j=i;j<=i+k-1;j++)
+            {
+              MaxArray[p]=arr[j];
+                p++;
+            }
+           //for getting maximum value from array
+           for(int l=0;l<MaxArray.Length;l++)
+            {
+                if(MaxArray[l]>max)
+                {
+                    max = MaxArray[l];
+                }
+            }
+            Console.Write(max+" ");
+            max = 0; p=0;
+            Array.Fill(MaxArray,0);
+        }
+    }
+}
