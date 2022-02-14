56b7bb439273bb698131897773a4f97bec9ec8ae
Mick1996# <mickykumar51@gmail.com>
2022-02-14 18:11:45 +0530

    Assignment 4

---
+++ Q1 of Assignment 4/Program.cs
@@ -0,0 +1,110 @@
+﻿class StringProblem
+{
+    public int CountChar(string str,char ch)
+    {
+        int result = 0;
+        char[] ch1 = str.ToCharArray();
+        for(int i=0;i<ch1.Length;i++)
+        {
+            if (ch1[i] == ch)
+                result++;
+        }
+        return result;
+    }
+    public void Substring(string str,int start,int end)
+    {
+         for(int i=start;i<end;i++)
+        {
+            for(int j=i;j<=end;j++)
+            {
+                for(int k=i;k<j+1;k++)
+                {
+                    Console.Write(str[k]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    public void splitstring(string str,char ch)
+    {
+        char [] ch1=str.ToCharArray();
+        for(int i=0;i<ch1.Length;i++)
+        {
+            if (ch1[i] == ch)
+            {
+                Console.WriteLine();
+                continue;
+            }
+            else
+                Console.Write(str[i]);
+        } 
+    }
+    public bool HasPattern(string str,string pattern)
+    {
+        int Flag = 0;
+        int len = str.Length;
+        int n = pattern.Length;
+        for(int i=0; i<=len-n;i++)
+        {
+            int k = i;
+            for(int j=0;j<n;j++)
+            {
+                if (str[k] != pattern[j])
+                {
+                    Flag = 0;
+                    break;
+                }
+                else
+                {
+                    Flag = 1;
+                    k++;
+                }
+            }
+            if (Flag == 1)
+                return true;
+        }
+        return false;
+    }
+    public bool AllwordsContainChar(string str,char ch)
+    {
+        int flag= 0;
+        string [] str1=str.Split(" ");
+        for(int i=0;i<str1.Length;i++)
+        {
+            string s=str1[i];
+            char [] ch1=s.ToCharArray();
+            for(int j=0;j<ch1.Length;j++)
+            {
+                if (ch1[j] == ch)
+                {
+                    flag = 1;
+                    break;
+                }
+                else
+                    flag = 0;
+            }
+            if (flag == 0)
+                return false;
+        }
+         return true;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        StringProblem p = new StringProblem();
+        int charoccurence=p.CountChar("mathematics",'m');
+        Console.WriteLine($"{charoccurence} times");
+        Console.WriteLine("Substring are:");
+        p.Substring("mathematics",1,6);
+        Console.WriteLine("string splitted");
+        p.splitstring("iliveinDelhicity",'i');
+        Console.WriteLine("\nchecking pattern Found or not");
+        bool result = p.HasPattern("bigohtech","iohte");
+        Console.WriteLine(result);
+        Console.WriteLine("checking all words containing given character or  not");
+        bool res= p.AllwordsContainChar("ic livce inc ountry",'c');
+        Console.WriteLine(res);
+    }
+}
---
+++ Q2 of Assignment 4/Program.cs
@@ -0,0 +1,19 @@
+﻿public class LengthLongestSubstr //Q2 0f Assignment 4
+{ 
+	  public static void Main()
+	{
+		string s = "abcabcbb";
+		int len = s.Length;
+		int result = 0;
+		int[] arr = new int[256]; //ascii value of every character is less than 256 
+		Array.Fill(arr, -1); //fill -1 at all position of array 
+		int i = 0;
+		for (int k = 0; k < len; k++)
+		{
+			i = Math.Max(i, arr[s[k]] + 1); //fetch value from ascii code then add 1.
+			result = Math.Max(result, k - i + 1);//hold longest substring length.
+			arr[s[k]] = k;      //transfering index of last character if character is repeating then increase i value.
+		}
+		Console.WriteLine(result);
+	}
+}
